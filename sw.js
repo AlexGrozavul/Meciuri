9d3fbe800647240df269cdf9fbcda60aa863302d
AlexGrozavul <grozavulalex@gmail.com>
2026-08-20 15:13:25 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,44 @@
+/* Service worker pentru "Meciurile mele".
+   Strategie: network-first, cu cache ca rezervă.
+   Asta înseamnă că, atunci când ai internet, vezi întotdeauna ultima
+   versiune a datelor; iar când nu ai, aplicația funcționează din cache. */
+
+const CACHE = 'meciurile-mele-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './data.js',
+  './manifest.webmanifest',
+  './icon-192.png',
+  './icon-512.png',
+  './icon-maskable-512.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE)
+      .then(c => c.addAll(ASSETS).catch(() => {}))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    fetch(e.request)
+      .then(res => {
+        const copy = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, copy)).catch(() => {});
+        return res;
+      })
+      .catch(() => caches.match(e.request).then(hit => hit || caches.match('./index.html')))
+  );
+});
